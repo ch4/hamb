989d9b14585166a2be4ce1c24875cc7e47214a4f
Mike <mike.lagoma@gmail.com>
2016-09-25 07:15:24 -0700

    allow access control

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(db.url);
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'localhost');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
