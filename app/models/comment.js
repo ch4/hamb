9333f130f9e5a848968c320f11e10c8888393994
mikelagoma <mike.lagoma@gmail.com>
2016-09-24 22:32:47 -0700

    Added needs API

--- app/models/comment.js
+++ app/models/comment.js
@@ -2,8 +2,9 @@
 // grab the mongoose module
 var mongoose = require('mongoose');
 
-// define our nerd model
+// define our user model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Nerd', {
-    name : {type : String, default: ''}
+module.exports = mongoose.model('Comment', {
+    need : {type : String, default: ''},
+    text : {type : String, default: ''}
 });
