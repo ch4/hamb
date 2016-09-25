0fcd8b5388f814d8d56053bedf37a97c57d40a93
mikelagoma <mike.lagoma@gmail.com>
2016-09-25 00:19:02 -0700

    comment model created

--- app/models/comment.js
+++ app/models/comment.js
@@ -6,5 +6,6 @@ var mongoose = require('mongoose');
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Comment', {
     need : {type : String, default: ''},
+    user : {type : String, default: 'Anonymous'},
     text : {type : String, default: ''}
 });
